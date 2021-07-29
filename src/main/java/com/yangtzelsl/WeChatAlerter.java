ede1e8ce3ab596d3cee3e62ddd7453041763f5c4
luis.liu <yangtzelsl@126.com>
2021-07-29 16:00:53 +0800

    更新azkaban自定义企业微信机器人告警

--- src/main/java/com/yangtzelsl/WeChatAlerter.java
+++ src/main/java/com/yangtzelsl/WeChatAlerter.java
@@ -5,13 +5,14 @@ import azkaban.executor.ExecutableFlow;
 import azkaban.executor.Executor;
 import azkaban.executor.ExecutorManagerException;
 import azkaban.sla.SlaOption;
-import azkaban.utils.AbstractMailer;
-import azkaban.utils.EmailMessageCreator;
 import azkaban.utils.Props;
 import cn.gjing.http.HttpClient;
 import cn.gjing.http.HttpMethod;
+import com.google.gson.JsonObject;
 import org.apache.log4j.Logger;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -21,51 +22,63 @@ import java.util.List;
  * @Date: 2021/7/7 19:38
  * @Version 1.0
  */
-public class WeChatAlerter extends AbstractMailer implements Alerter {
+public class WeChatAlerter implements Alerter {
     private static final Logger logger = Logger.getLogger(WeChatAlerter.class);
 
-    private final String appKey;
-    private final String url;
+    private final String wechatKey;
 
-    public WeChatAlerter(Props props, EmailMessageCreator messageCreator) {
-        super(props, messageCreator);
-        appKey = props.getString("my.alert.appKey", "");
-        url = props.getString("my.alert.url", "");
-        logger.info("Appkey: " + appKey);
-        logger.info("URL: " + url);
+    public WeChatAlerter(Props props) {
+        wechatKey = props.getString("alert.wechatKey", "");
+        logger.info("wechatKey: " + wechatKey);
     }
 
     /**
      * 成功的通知
      *
-     * @param exflow
+     * @param exFlow
      * @throws Exception
      */
     @Override
-    public void alertOnSuccess(ExecutableFlow exflow) throws Exception {
+    public void alertOnSuccess(ExecutableFlow exFlow) throws Exception {
         logger.info("任务成功了，恭喜您！");
     }
 
     /**
      * 出现问题的通知
      *
-     * @param exflow
+     * @param exFlow
      * @param extraReasons
      * @throws Exception
      */
     @Override
-    public void alertOnError(ExecutableFlow exflow, String... extraReasons) throws Exception {
+    public void alertOnError(ExecutableFlow exFlow, String... extraReasons) throws Exception {
 
         // 企业微信API: https://work.weixin.qq.com/api/doc/90000/90136/91770
 
         //一般来说网络电话服务都是通过HTTP请求发送的，这里可以调用shell发送HTTP请求
-        /*
+
+        logger.info("任务失败了，请及时处理...");
+
+        String flowName = exFlow.getFlowName();
+        String projectName = exFlow.getProjectName();
+        String executionId = String.valueOf(exFlow.getExecutionId());
+        String failTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH:mm:ss"));
+
+        String url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + wechatKey;
+        String markdownMessage = "{\n" +
+                "    \"msgtype\": \"markdown\",\n" +
+                "    \"markdown\": {\n" +
+                "        \"content\": \"<font color=\\\"warning\\\">任务失败请相关同事注意!</font>\n" +
+                "         >项目名称: <font color=\\\"comment\\\">"+projectName+"</font>\n" +
+                "         >工作流名称: <font color=\\\"comment\\\">"+flowName+"</font>\n" +
+                "         >执行序号: <font color=\\\"comment\\\">"+executionId+"</font>\n" +
+                "         >任务失败时间: <font color=\\\"comment\\\">"+failTime+"</font>\"\n" +
+                "    }\n" +
+                "}";
+
+        logger.info("发送的消息体内容为：" + markdownMessage);
+
         JsonObject alert = new JsonObject();
-        alert.addProperty("app", appKey);
-        alert.addProperty("eventId", exflow.getId());
-        alert.addProperty("eventType", "trigger");
-        alert.addProperty("alarmContent", exflow.getId() + " fails!");
-        alert.addProperty("priority", "2");
         String[] cmd = new String[8];
         cmd[0] = "curl";
         cmd[1] = "-H";
@@ -73,32 +86,23 @@ public class WeChatAlerter extends AbstractMailer implements Alerter {
         cmd[3] = "-X";
         cmd[4] = "POST";
         cmd[5] = "-d";
-        cmd[6] = alert.toString();
+        cmd[6] = markdownMessage;
         cmd[7] = url;
-        System.out.println(cmd.toString());
-         logger.info("Sending wechat robot alert!");
+
+        logger.info("发送请求的消息体是："+cmd.toString());
         Runtime.getRuntime().exec(cmd);
-        */
 
-        logger.info("任务失败了，请及时处理...");
-
+        // 注意：目前使用HTTP方式会发生故障，具体体现为：第一次失败可以发送告警，后续job运行状态一直为preparing状态，会跳过执行
         // 使用HTTP的方式发送请求
-        String url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=abc335a5-xxxx-xxxx-bb8b-61c1b72a79b7";
-        String ss = "{\n" +
-                "    \"msgtype\": \"text\",\n" +
-                "    \"text\": {\n" +
-                "        \"content\": \"广州今日天气111：29度，大部分多云，降雨概率：60%\",\n" +
-                "        \"mentioned_list\":[\"wangqing\",\"@all\"],\n" +
-                "        \"mentioned_mobile_list\":[\"13800001111\",\"@all\"]\n" +
-                "    }\n" +
-                "}\n";
-        logger.info("发送的消息体内容为："+ss);
-        String str = HttpClient.builder(url, HttpMethod.POST, String.class)
-                .body(ss)
+
+        /*
+        String responseMessage = HttpClient.builder(url, HttpMethod.POST, String.class)
+                .body(markdownMessage)
                 .execute()
                 .get();
-        logger.info("返回的结果集内容为："+str);
-        System.out.println(str);
+
+        logger.info("返回的结果集内容为：" + responseMessage);
+        */
     }
 
     /**
